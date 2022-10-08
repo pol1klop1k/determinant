75992305f0993d90e1beea926fb8a6be36f0c5f1
pol1klop1k <garik.b.04@gmail.com>
2022-10-08 19:39:24 +0300

    finished minor func

--- solver.py
+++ solver.py
@@ -1,19 +1,20 @@
-r = int(input())
-num = [[] for k in range(r+1)]
-for k in range(1,r+1):
-    num[k].append('')
-    for n in range(r):
-        num[k].append(input())
+import math
 
-def alDop(i,j):
+def create(value, nums):        #функция, которая создает определитель
+    nums = iter(nums)
+    deter = [[] for k in range(value+1)]        #само тело определителя
+    for k in range(1, value+1):
+        deter[k].append('')       #создаю пустой элемент в каждой строке определителя для удобности индексов
+        for n in range(value):
+            deter[k].append(next(nums))     #добавляю в строку определителя элементы
+    return deter
+
+def minor(deter,i,j):           #функция, которая возвращает минор по строке i и столбцу j
     new = [[]]
-    for k in range(1,r+1):
-        print(k, j)
-        if k == i:
-            continue
-        else:
-            new.append(num[k])
-            new[k][j] = 0
+    for k in range(1, len(deter)):
+        new.append([''])
+        for n in range(1, len(deter)):
+            new[k].append(deter[k][n])
+        new[k].pop(j)       #удаляем j элемент из каждой строки
+    new.pop(i)              #удаляем i строку
     return new
-
-print(alDop(2,2))
