835c208301a4e235a44d2b33b5970e90e3613ca9
pol1klop1k <garik.b.04@gmail.com>
2022-10-10 19:47:43 +0300

    added res func

--- solver.py
+++ solver.py
@@ -18,3 +18,16 @@ def minor(deter,i,j):           #функция, которая возвраща
         new[k].pop(j)       #удаляем j элемент из каждой строки
     new.pop(i)              #удаляем i строку
     return new
+
+def alg(deter, i, j):       #функция, которая вычисляет элемент со своим знаком i и j
+    factor = deter[i][j]
+    return factor * (-1)**(i+j)
+
+def res(rem, cur=0):        #рекурсивная функция, которая считает определитель
+    if len(rem) > 2:
+        for i in range(1, len(rem)):
+            cur += alg(rem, 1, i) * res(minor(rem, 1, i))       #заходим в рекурсию
+        return cur
+    else:
+        cur = rem[1][1]     #возвращаем число, если остался определитель 1x1
+        return cur
